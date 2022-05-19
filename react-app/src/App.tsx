e7bc4743293ff2ba5fa7f9644349253fbd066ce0
bestGao <1055652678@qq.com>
2022-05-19 14:45:50 +0800

    feat(m): 新增vue-app

---
+++ react-app/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.less";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">react子应用 测试</header>
+    </div>
+  );
+}
+
+export default App;
